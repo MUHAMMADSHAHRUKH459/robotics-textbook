032c8ea2d472a09303417137cd33fc01296bf883
MUHAMMADSHAHRUKH459 <freelacing123@gmail.com>
2025-12-07 06:08:03 +0500

    Configure for Vercel deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,9 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://MUHAMMADSHAHRUKH459.github.io',
+  url: 'https://robotics-textbook.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/robotics-textbook/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
